e4aa7c2896bb56d1a69cff4a0bb990284eb4db7d
Chadric Nathan <chadric.nathan@gmail.com>
2021-10-31 23:33:30 -0500

    Ex3 Attempted for lab4

--- app.js
+++ app.js
@@ -1,49 +1,89 @@
 window.onload=function(){
-    var button = document.querySelector("button");
-    var httpRequest;
 
+  var httpRequest = new XMLHttpRequest();
+  var button = document.querySelector("button");
+  var url = "http://localhost/info2180-lab4/superheroes.php";
+  //httpRequest.onreadystatechange = getSupe;
+  //httpRequest.open('GET', url);
+  //httpRequest.send();
+  /*if (httpRequest.readyState === XMLHttpRequest.DONE) {
+    if (httpRequest.status === 200) {
+      var response = httpRequest.responseText;
+      document.getElementById("Result").innerHTML= response;
+      //alert(response);
+    } else {
+      document.getElementById("Result").innerHTML= "Error";
+    }
+  }*/
 
-    button.addEventListener("click",function(element) {
-    element.preventDefault();
-    ///*
-    httpRequest = new XMLHttpRequest();
-
-    // GET Request
-    var url = "http://localhost/info2180-lab4/superheroes.php";
+  button.addEventListener("click",function(element) {
+  element.preventDefault();
+  var name= document.getElementById("frm1");
+  if(name==""){
+    document.getElementById("Result").innerHTML= "SUPERHERO NOT FOUND";
+  }
+  else{
     httpRequest.onreadystatechange = getSupe;
-    httpRequest.open('GET', url);
-    httpRequest.send();
-  });
-
-  function getSupe() {
+  httpRequest.open('GET', url);
+  httpRequest.send();    
     if (httpRequest.readyState === XMLHttpRequest.DONE) {
-      if (httpRequest.status === 200) {
-        var response = httpRequest.responseText;
-        alert(response);
-      } else {
-        alert('There was a problem with the request.');
+          if (httpRequest.status === 200) {
+            var response = httpRequest.responseText;
+            document.getElementById("Result").innerHTML= response;
+        //alert(response);
+          } else {
+              document.getElementById("Result").innerHTML= "Error";
       }
     }
   }
+  
+  //superheroes.php?query=name
+
+
+  /*
+  if (httpRequest.readyState === XMLHttpRequest.DONE) {
+    if (httpRequest.status === 200) {
+      var response = httpRequest.responseText;
+      document.getElementById("Result").innerHTML= response;
+      //alert(response);
+    } else {
+      document.getElementById("Result").innerHTML= "Error";
+    }
+  }*/
+
+  // GET Request
+  
+});
+
+function getSupe() {
+  if (httpRequest.readyState === XMLHttpRequest.DONE) {
+    if (httpRequest.status === 200) {
+      var response = httpRequest.responseText;
+      document.getElementById("Result").innerHTML= response;
+      //alert(response);
+    } else {
+      document.getElementById("Result").innerHTML= "Error";
+    }
+  } 
+}
 
 }
- // Simple Example using jQuery's $.ajax() method
- /*$(document).ready(function() {
-    var xmlBtn = $('#getXml');
-
-    xmlBtn.on('click', function() {
-        $.ajax('superheroes.php', {
-            method: "GET",
-            dataType: 'xml'
-        }).done(function(response) {
-            var people = $(response).find('superhero');
-            $('#results').append("<ul></ul>");
-            $(people).each(function() {
-                $('#results ul').append('<li>' + $(this).find($superhero['alias']).text() + '</li>');
-            });
-        }).fail(function() {
-            alert('There was a problem with the request.');
-        });
-    });
+// Simple Example using jQuery's $.ajax() method
+/*$(document).ready(function() {
+  var xmlBtn = $('#getXml');
+  xmlBtn.on('click', function() {
+      $.ajax('superheroes.php', {
+          method: "GET",
+          dataType: 'xml'
+      }).done(function(response) {
+          var people = $(response).find('superhero');
+          $('#results').append("<ul></ul>");
+          $(people).each(function() {
+              $('#results ul').append('<li>' + $(this).find($superhero['alias']).text() + '</li>');
+          });
+      }).fail(function() {
+          alert('There was a problem with the request.');
+      });
+  });
 });
 }*/
--- superheroes.php
+++ superheroes.php
@@ -70,3 +70,39 @@ $superheroes = [
   <li><?= $superhero['alias']; ?></li>
 <?php endforeach; ?>
 </ul>
+
+<?php
+    header('Access-Control-Allow-Origin: *');
+?>
+
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    if (!isset($_POST['name']) or
+    empty($_POST['name'])) {
+      echo "What?!?!? You don't have a name or ID Number?";
+    } else {
+      echo "Your name is: <strong>" . $_POST['name'] . "</strong> and your ID Number is: <strong>" . $_POST['idnumber'] . "</strong>.";
+    }
+  }
+/*
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  if (isset($_POST['name']) && !empty($_POST['name'])) {
+  // if (isset($_GET['name']) && !empty($_GET['name'])) {
+
+    $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
+    // $name = filter_input(INPUT_GET, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
+    echo $name;
+
+    /*if (in_array($name, $superheroes)) {
+      echo json_encode(['data' => ['name' => $name]]);
+    } else {
+      echo json_encode(['data' => ['error' => "The name {$name} does not exist."]]);
+    }
+  } else {
+      header("HTTP/1.0 400 Bad Request");
+      echo json_encode(['data' => ['error' => 'Invalid Request']]);
+  }
+}*/
+?>
